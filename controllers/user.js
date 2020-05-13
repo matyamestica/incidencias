11c4f6054c283598621d005b55d82c4397a9b204
maty <maty6289@live.cl>
2020-05-13 17:03:18 -0400

    primeros controladores de usuario

--- controllers/user.js
+++ controllers/user.js
@@ -1,11 +1,39 @@
 'use strict'
 
+var bcrypt = require('bcrypt-nodejs');
+var User = require('../models/user');
+
+
 function pruebas(req, res){
     res.status(200).send({
         message: 'Probando una accion del controlador de usuario'
     });
 }
 
+function saveUser(req, res){
+    var user = new User();
+
+    var params = req.body;
+    
+    console.log(params);
+
+    user.name = params.name;
+    user.surname = params.surname;
+    user.email = params.email;
+    user.rut =  params.rut;
+    user.image = 'null';
+    user.role = 'ROLE_USER';
+
+    if(params.password){
+        //Encriptar contraseña y guardarla en la base de datos
+
+    }else{
+        res.status(500).send({message: 'Introduzca la contraseña'});
+    }
+
+}
+
 module.exports = {
-    pruebas
+    pruebas,
+    saveUser
 };
