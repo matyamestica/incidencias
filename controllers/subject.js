c96de0a36a81041fe13245e7feb6b718de24d575
maty <maty6289@live.cl>
2020-05-19 18:28:42 -0400

    arreglando problemas en los modelos de la base de datos

---
+++ controllers/subject.js
@@ -0,0 +1,5 @@
+'use strict'
+//librerias para trabajar con el sistema de fichero, para subir archivos
+var path = require('path');
+var fs = require('fs');
+
--- models/subject.js
+++ models/subject.js
@@ -6,7 +6,8 @@ var Schema = mongoose.Schema;
 var SubjectSchema = Schema({
     name: String,
     code: String,
-    teacher: {type: Schema.ObjectId, ref:'User'}
+    teacher: {type: Schema.ObjectId, ref:'User'},
+    file: String
 });
 
 module.exports = mongoose.model('Subject', SubjectSchema);
