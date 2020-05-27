bae13884c7e7ca4bb04b4c69b2e687e7e36bea5f
maty <maty6289@live.cl>
2020-05-27 17:17:01 -0400

    arreglando problemas

---
+++ app.js
@@ -0,0 +1,21 @@
+'use strict'
+
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+//cargar rutas
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json);
+
+//configurar cabeceras http
+
+//rutas base
+
+app.get('/pruebas', function(){
+    res.status(200).send({message: 'probando incidencias'});
+});
+module.exports = app;
+
