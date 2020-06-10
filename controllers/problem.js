91818db38ab6a09347816028a51140bba6f16c4c
Mcastillo007 <marco.cgonzalez19@gmail.com>
2020-06-10 01:10:59 -0400

    Creando Controlador Problem
    Ya se puede crear un problema, se modificó el modelo Problem, ya que por las referencias existentes arroja un error.

--- controllers/problem.js
+++ controllers/problem.js
@@ -12,6 +12,38 @@ function getProblem(req, res){
     res.status(200).send({message: 'Método getProblem del controlador Problem'});
 }
 
+function saveProblem(req, res){
+    var problem = new Problem();
+
+    var params = req.body;
+    console.log(params);
+
+    problem.code = params.code;
+    problem.user_create = params.user_create;
+    problem.description = params.description;
+    problem.user_secretary = 'null';//MODIFICAR PARA OBTENER ID SECRETARIA
+    problem.user_director = 'null';//MODIFICAR PARA OBTENER ID DIRECTOR
+    problem.file = 'null'; //MODIFICAR PARA PODER SUBIR ARCHIVO
+    problem.subject = params.subject;
+    problem.category = params.category;
+    problem.state = params.state;
+    problem.date_create = 'null';//MODIFICAR PARA OBTENER FECHA DE CREACIÓN
+    problem.date_fin = 'null';//MODIFICAR PARA OBTENER FECHA DE CIERRE
+
+    problem.save((err, problemStored) => {
+        if(err){
+            res.status(500).send({message:'Error al ingresar problema'});
+        }else{
+            if(!problemStored){
+                res.status(404).send({message:'El error no ha sido ingresado'});
+            }else{
+                res.status(200).send({problem: problemStored});
+            }
+        }
+    });
+}
+
 module.exports = {
-    getProblem
+    getProblem,
+    saveProblem
 }
