0759ce61ae505c1b6256be9548ca80907cf7441a
maty <maty6289@live.cl>
2020-06-10 19:10:04 -0400

    Revert "Creando Controlador Problem"
    This reverts commit 91818db38ab6a09347816028a51140bba6f16c4c.

--- api/routes/problem.js
+++ api/routes/problem.js
@@ -6,6 +6,5 @@ var api = express.Router();
 var md_auth = require('../middlewares/authenticated');
 
 api.get('/problem', md_auth.ensureAuth, ProblemController.getProblem);
-api.post('/problem', md_auth.ensureAuth, ProblemController.saveProblem);
 
 module.exports = api;
