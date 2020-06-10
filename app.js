0348ad48c1e2c88b75600e448e8e10a3146ddfe6
maty <maty6289@live.cl>
2020-06-10 19:38:49 -0400

    oli

--- app.js
+++ app.js
@@ -6,25 +6,12 @@ var bodyParser = require('body-parser');
 var app = express();
 
 //cargar rutas
-var user_routes = require('./routes/user');
-var subject_routes = require('./routes/subject');
-var subcategory_routes = require('./routes/subcategory');
-var problem_routes = require('./routes/problem');
-var category_routes = require('./routes/category');
-var answer_routes = require('./routes/answer');
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 //configurar cabeceras http
 
-//Rutas base
-app.use('/api', user_routes);
-app.use('/api', subject_routes);
-app.use('/api', subcategory_routes);
-app.use('/api', problem_routes);
-app.use('/api', category_routes);
-app.use('/api', answer_routes);
-
+//tutas base
 
 module.exports = app;
