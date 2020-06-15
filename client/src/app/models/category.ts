1c09588d0a9accba1b7ce9f5508eab6d15dc0db4
maty <maty6289@live.cl>
2020-06-14 22:32:45 -0400

    creacion de modelos frontend

---
+++ client/src/app/models/category.ts
@@ -0,0 +1,9 @@
+export class Problem{
+  constructor(
+    public _id: string,
+    public name: string,
+    public description: string,
+    public date: string,
+    public file: string
+  ){}
+}
--- client/src/app/models/user.ts
+++ client/src/app/models/user.ts
@@ -1,5 +1,14 @@
 export class User{
   constructor(
-    public _id: string
+    public _id: string,
+    public name: string,
+    public surname: string,
+    public password: string,
+    public rut: string,
+    public image: string,
+    public year: number,
+    public role: string,
+    public date: string,
+    public state: string
   ){}
 }
