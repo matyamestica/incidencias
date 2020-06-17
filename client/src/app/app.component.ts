33b90219a2ecd078ec8ad30717521282a42327f3
maty <maty6289@live.cl>
2020-06-16 20:54:31 -0400

    login funcionando

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -13,6 +13,7 @@ export class AppComponent  implements OnInit{
   public user: User;
   public identity;
   public token;
+  public errorMessage;
 
   constructor(
     private _userService: UserService
@@ -27,18 +28,58 @@ export class AppComponent  implements OnInit{
 
   public onSubmit(){
     console.log(this.user);
+
+    //conseguir datos del usuario identificado
     this._userService.signup(this.user).subscribe(
       response => {
-        console.log(response);
+        let identity = response.user;
+        this.identity = identity;
+
+        if(!this.identity._id){
+          alert("El usuario no se ha registrado correctamente");
+        }else{
+          //crear elemento en el local storage para tener al usuario en sesion
+
+          //conseguir tokken para enviarselo a cada peticion http
+
+          this._userService.signup(this.user, 'true').subscribe(
+            response => {
+              let token = response.token;
+              this.token = token;
+
+              if(!this.token.length <= 0){
+                alert("El token no se ha generado");
+              }else{
+                  //crear elemento en el localstorage para tener token disponible
+                  console.log(token);
+                  console.log(identity);
+              }
+            },
+            error => {
+              var errorMessage = <any>error;
+
+              if(errorMessage != null){
+                var body = JSON.parse(error._body);
+                this.errorMessage = body.message;
+                console.log(error);
+              }
+            }
+          );
+
+
+
+
+        }
       },
       error => {
         var errorMessage = <any>error;
 
         if(errorMessage != null){
+          var body = JSON.parse(error._body);
+          this.errorMessage = body.message;
           console.log(error);
         }
       }
-
-    )
+    );
   }
 }
