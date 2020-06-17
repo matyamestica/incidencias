8e1f2fc9122e5c808a3752f18f7321be145e7328
Mcastillo007 <marco.cgonzalez19@gmail.com>
2020-06-16 20:10:03 -0400

    Restauración de rutas

--- api/app.js
+++ api/app.js
@@ -6,6 +6,12 @@ var bodyParser = require('body-parser');
 var app = express();
 
 //cargar rutas
+var user_routes = require('./routes/user');
+var subject_routes = require('./routes/subject');
+var subcategory_routes = require('./routes/subcategory');
+var problem_routes = require('./routes/problem');
+var category_routes = require('./routes/category');
+var answer_routes = require('./routes/answer');
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
@@ -21,9 +27,11 @@ app.use((req, res, next) => {
 });
 
 
-//rutas base
+//Rutas base
 app.use('/api', user_routes);
-
-
-
+app.use('/api', subject_routes);
+app.use('/api', subcategory_routes);
+app.use('/api', problem_routes);
+app.use('/api', category_routes);
+app.use('/api', answer_routes);
 module.exports = app;
