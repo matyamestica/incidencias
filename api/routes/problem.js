958c1d8ad2f0101827f02f74deddaa119e9314e4
maty <maty6289@live.cl>
2020-06-23 04:45:17 -0400

    actualizacion de formularios
    Co-Authored-By: barbarasuasep <barbarasuasep@users.noreply.github.com>

--- api/routes/problem.js
+++ api/routes/problem.js
@@ -6,5 +6,6 @@ var api = express.Router();
 var md_auth = require('../middlewares/authenticated');
 
 api.get('/problem', md_auth.ensureAuth, ProblemController.getProblem);
+api.post('/problem', md_auth.ensureAuth, ProblemController.saveProblem);
 
 module.exports = api;
