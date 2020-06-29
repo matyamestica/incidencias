b0a148e40403de5c8313587059acb6bc7aa5f7b7
Mcastillo007 <marco.cgonzalez19@gmail.com>
2020-06-29 02:16:15 -0400

    Push

---
+++ client/src/app/services/subcategory.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
+import { GLOBAL } from './global';
+import { Subcategory } from './../models/subcategory';
+
+@Injectable()
+export class SubcategoryService{
+  public url: string;
+  public identity;
+  public token;
+
+  constructor(private _http: Http){
+    this.url = GLOBAL.url;
+  }
+  
+  addSubcategory(token, subcategory: Subcategory){
+    let params = JSON.stringify(subcategory);
+    let headers = new Headers({
+        'Content-type':'application/json',
+        'Authorization':token
+    });
+
+    return this._http.post(this.url+'subcategory',params, {headers: headers})
+                     .map(res => res.json());
+  }
+}
